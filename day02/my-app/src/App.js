31cbb7cd58afd1030528c8edfc66598d8ea01f68
rifatabrarjowad <rajdiprifat2004@gmail.com>
2021-09-19 18:09:06 +0600

    error

--- day02/my-app/src/App.js
+++ day02/my-app/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, world!</h1>,
+      const name = "Rifat Abrar Jowad";
+      <h1>Hello, {name}</h1>;
     </div>
   );
 }
